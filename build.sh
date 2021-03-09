08148fb9b0b9ca571ea8acde817c24d02d935fe4
Christoph Bühler <cbuehler@rootd.ch>
2021-03-09 16:04:08 +0100

    adjust building with plantuml

--- build.sh
+++ build.sh
@@ -6,29 +6,31 @@ set -euo pipefail
 
 PLANTUML_BIN=/c/Users/cbueh/scoop/shims/plantuml.cmd pandoc \
     --filter pandoc-xnos \
-    --filter pandoc-plantuml \
-    --lua-filter=../lib/lua-filters/scholarly-metadata/scholarly-metadata.lua \
-    --lua-filter=../lib/lua-filters/author-info-blocks/author-info-blocks.lua \
+    --lua-filter=lib/custom/plantuml-converter.lua \
+    --lua-filter=lib/lua-filters/short-captions/short-captions.lua \
+    --lua-filter=lib/lua-filters/scholarly-metadata/scholarly-metadata.lua \
+    --lua-filter=lib/lua-filters/author-info-blocks/author-info-blocks.lua \
     --metadata-file=./metadata.yaml \
     --citeproc \
     --bibliography=./bibliography.bib \
     --toc \
     --standalone \
-    --output=../public/index.html \
+    --output=public/index.html \
     ./sections/*.md
 
 # PLANTUML_BIN=/c/Users/cbueh/scoop/shims/plantuml.cmd pandoc \
 #     --filter pandoc-xnos \
-#     --filter pandoc-plantuml \
-#     --lua-filter=../lib/lua-filters/scholarly-metadata/scholarly-metadata.lua \
-#     --lua-filter=../lib/lua-filters/author-info-blocks/author-info-blocks.lua \
+#     --lua-filter=lib/custom/plantuml-converter.lua \
+#     --lua-filter=lib/lua-filters/short-captions/short-captions.lua \
+#     --lua-filter=lib/lua-filters/scholarly-metadata/scholarly-metadata.lua \
+#     --lua-filter=lib/lua-filters/author-info-blocks/author-info-blocks.lua \
 #     --metadata-file=./metadata.yaml \
 #     --citeproc \
 #     --bibliography=./bibliography.bib \
 #     --toc \
 #     --standalone \
-#     --output=../public/report.pdf \
+#     --output=public/report.pdf \
 #     ./sections/*.md
 
-cp -R images ../public/images
-cp -R plantuml-images ../public/plantuml-images
+cp -R images public/images
+cp -R diagrams public/diagrams
